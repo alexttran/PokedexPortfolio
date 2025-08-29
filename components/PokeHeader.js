65f8d0a599619752ada8f96c207553d0ac4b764c
Alex Tran <alexttran25@gmail.com>
2025-08-28 21:21:56 -0700

    Added a header

---
+++ components/PokeHeader.js
@@ -0,0 +1,10 @@
+export default function PokeHeader() {
+  return (
+    <div className="w-full bg-poke-600 text-white">
+      <div className="section py-6 flex items-center gap-4">
+        <div className="w-4 h-4 rounded-full bg-white/90 border border-black/20" />
+        <h1 className="text-4xl tracking-widest">Pokédex Portfolio</h1>
+      </div>
+    </div>
+  );
+}
